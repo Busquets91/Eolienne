ded99270994aab25641dd13fe99987cec046df46
busquets <alexis.pointurier@gmail.com>
2016-10-20 12:04:46 +0200

    Premiers styles

--- Project/app/src/main/java/app4_project/eolienne/MainActivity.java
+++ Project/app/src/main/java/app4_project/eolienne/MainActivity.java
@@ -2,12 +2,16 @@ package app4_project.eolienne;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        TextView text = new TextView(this);
+        text.setText("Bonjour, vous me devez 1 000 000€.");
+        setContentView(text);
     }
 }
