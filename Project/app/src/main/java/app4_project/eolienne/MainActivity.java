7e2c3e902d72b3dac3eb265b2eca7f700f92e71e
busquets <alexis.pointurier@gmail.com>
2016-10-20 12:28:43 +0200

    Fin premier cours

--- Project/app/src/main/java/app4_project/eolienne/MainActivity.java
+++ Project/app/src/main/java/app4_project/eolienne/MainActivity.java
@@ -2,16 +2,12 @@ package app4_project.eolienne;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TextView text = new TextView(this);
-        text.setText("Bonjour, vous me devez 1 000 000€.");
-        setContentView(text);
+        setContentView(R.layout.activity_main);
     }
 }
