9b2ba7c292a8ef78e7e37ea577836d003ece0cb7
Anuswan91 <vivien.blum@free.fr>
2017-02-09 10:00:00 +0100

    extrem commit

---
+++ Project/app/src/main/java/com/example/vivien/testandroid/BluetoothWindmill.java
@@ -0,0 +1,40 @@
+package com.example.vivien.testandroid;
+
+/**
+ * Created by Vivien on 08/12/2016.
+ */
+
+import android.bluetooth.BluetoothAdapter;
+//import android.widget.Toast;
+
+public class BluetoothWindmill {
+    protected boolean isInitialized = false;
+    protected BluetoothAdapter bluetoothAdapter;
+
+    protected void enableBluetooth(){
+        if (!bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.enable();
+        }
+    }
+
+    public boolean init(){
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            isInitialized = false;
+            //TODO pas de bluetooth
+            return false;
+        }
+        else {
+            //TODO init bluetooth
+            enableBluetooth();
+            isInitialized = true;
+            return true;
+        }
+    }
+
+    public boolean connectToModule(){
+        return true;
+    }
+
+
+}
